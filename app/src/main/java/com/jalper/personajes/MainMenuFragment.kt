91ca8128702d06e4fb722a213b592e8f794cbe62
Ridley <gonzalezfranks@hotmail.com>
2023-10-09 21:16:55 +0200

    Main menu preparado para cargas fragmento de juego

--- app/src/main/java/com/jalper/personajes/MainMenuFragment.kt
+++ app/src/main/java/com/jalper/personajes/MainMenuFragment.kt
@@ -7,9 +7,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.jalper.personajes.databinding.FragmentMainMenuBinding
 import com.jalper.personajes.model.PreferenceKeys
+import com.jalper.personajes.model.ResourceState
+import com.jalper.personajes.model.viewmodel.CharacterListState
+import com.jalper.personajes.model.viewmodel.CharactersViewModel
 
 class MainMenuFragment : Fragment() {
 
@@ -20,6 +24,8 @@ class MainMenuFragment : Fragment() {
     private var user_name: String = ""
 
     private var preferences: SharedPreferences? = null
+
+    private lateinit var charactersViewModel: CharactersViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,9 +38,45 @@ class MainMenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initViewModel()
         initUI()
     }
 
+    private fun initViewModel(){
+        charactersViewModel = ViewModelProvider(requireActivity()).get(CharactersViewModel::class.java)
+
+        charactersViewModel.getCharacterLiveData().observe(viewLifecycleOwner){state ->
+            handleCharacterListState(state)
+        }
+    }
+
+    private fun handleCharacterListState(state: CharacterListState){
+        when(state){
+            is ResourceState.Loading ->{
+                binding.pbMainMenu.visibility = View.VISIBLE
+                binding.btnMainMenuJugar.visibility = View.GONE
+                binding.btnMainMenuLista.visibility = View.GONE
+                binding.btnHistorico.visibility = View.GONE
+            }
+
+            is ResourceState.Success ->{
+                binding.pbMainMenu.visibility = View.GONE
+                binding.btnMainMenuJugar.visibility = View.VISIBLE
+                binding.btnMainMenuLista.visibility = View.VISIBLE
+                binding.btnHistorico.visibility = View.VISIBLE
+
+                //Aqui tenemos que recibir el resultado de la consulta a la API
+                // y probablemente sacar las preguntas
+            }
+
+            is ResourceState.Error ->{
+                binding.pbMainMenu.visibility = View.GONE
+                binding.btnMainMenuJugar.visibility = View.VISIBLE
+                binding.btnMainMenuLista.visibility = View.VISIBLE
+                binding.btnHistorico.visibility = View.VISIBLE
+            }
+        }
+    }
 
     private fun initUI(){
         //Seteamos nombre en la parte superior
@@ -43,6 +85,10 @@ class MainMenuFragment : Fragment() {
         binding.tvNombrePlayer.text = user_name
 
         //Seteamos navegacion en botones
+        binding.btnMainMenuJugar.setOnClickListener{
+            findNavController().navigate(R.id.action_mainMenuFragment_to_gameFragment)
+        }
+
         binding.btnMainMenuLista.setOnClickListener{
             findNavController().navigate(R.id.action_mainMenuFragment_to_listaPersonajesFragment)
         }
--- app/src/main/java/com/jalper/personajes/presentation/fragment/GameFragment.kt
+++ app/src/main/java/com/jalper/personajes/presentation/fragment/GameFragment.kt
@@ -6,14 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jalper.personajes.R
+import com.jalper.personajes.databinding.FragmentGameBinding
 
 class GameFragment : Fragment(){
+
+    private val binding: FragmentGameBinding by lazy {
+        FragmentGameBinding.inflate(layoutInflater)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View?{
         return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    private fun initUI(){
+
+        //Tenemos que obtener un objeto de la clase
+
     }
 
 }
