e33ffd76389d9186685660087fdfa06ef092cb17
Ridley <gonzalezfranks@hotmail.com>
2023-10-12 14:26:33 +0200

    Preparado para incluir BD

--- app/src/main/java/com/jalper/personajes/WrongFragment.kt
+++ app/src/main/java/com/jalper/personajes/WrongFragment.kt
@@ -5,55 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.jalper.personajes.databinding.FragmentWrongBinding
+import com.jalper.personajes.model.viewmodel.CharactersViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [WrongFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WrongFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val binding: FragmentWrongBinding by lazy {
+        FragmentWrongBinding.inflate(layoutInflater)
     }
+
+    private lateinit var charactersViewModel: CharactersViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wrong, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WrongFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WrongFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViewModel()
+        initUI()
     }
+
+    private fun initViewModel(){
+        //inicializamos view model
+        charactersViewModel = ViewModelProvider(requireActivity()).get(CharactersViewModel::class.java)
+
+        //guardamos resultados en algun lado, por ejemplo player prefs
+
+        //ponemos puntuacion a 0
+        charactersViewModel.setPuntuation(0)
+    }
+
+    private fun initUI(){
+        //Seteamos botones de intentar otra vez y saliar
+        binding.btnExitGame.setOnClickListener{
+            findNavController().navigate(R.id.action_wrongFragment_to_mainMenuFragment)
+        }
+
+        binding.btnTryAgain.setOnClickListener{
+
+            findNavController().navigate(R.id.action_wrongFragment_to_gameFragment)
+        }
+    }
+
 }
